8a15b9de9b5f1b68c8e19065c4f2ac397990ba61
Balmukund Jaiswar <108282255+Mukund-Jaiswar@users.noreply.github.com>
2024-10-29 12:37:14 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,6 @@ import {getFirestore} from "@firebase/firestore";
 import { getAuth, GoogleAuthProvider} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3zaP2G83GITyOCpmN_lzAjfI9QSLNhXk",
-  authDomain: "random-test-4041a.firebaseapp.com",
-  projectId: "random-test-4041a",
-  storageBucket: "random-test-4041a.appspot.com",
-  messagingSenderId: "97729673470",
-  appId: "1:97729673470:web:bf78a822a2a55f46b1cdaf"
 };
 
 // Initialize Firebase
